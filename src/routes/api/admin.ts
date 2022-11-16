b816e12c99e61746ab12515584b72167ad512011
Ryan O'Connor <ryan.j.oconnor@vista.co>
2022-11-16 22:18:39 +1300

    update admin routes. Untested.

--- src/routes/api/admin.ts
+++ src/routes/api/admin.ts
@@ -2,8 +2,7 @@ import { ethers } from 'ethers';
 import { alchemyClient } from './../../../config/alchemy';
 import express, { Request, Response } from 'express';
 import HttpStatusCodes from 'http-status-codes';
-import { Contract, Utils, Wallet } from 'alchemy-sdk';
-const  abi = require('../../../config/abi.json');
+const abi = require('../../../config/abi.json');
 
 const router = express.Router();
 
@@ -27,43 +26,38 @@ router.get('/token-quantities', async (req: Request, res: Response) => {
   }
 });
 
-// @route   GET api/admin/balance
-// @desc    Get balance
-// @access  Public
-router.get('/balance', async (req: Request, res: Response) => {
-  try {
-    const data = 'balance placeholder'
-    res.json(data);
-  } catch (err: any) {
-    console.error(err.message);
-    res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send('Server Error');
-  }
-});
-
 // @route   POST api/transfer?contractId={contractId}&vip={boolean}&accountId={id}
 // @desc    Get balance quantity
 // @access  Private (but not really)
 router.get('/transfer', async (req: Request, res: Response) => {
   try {
+    const contractAddress = req.query['address'];
+    const accountId = req.query['accountId'];
+
+    if(typeof contractAddress !== 'string' || typeof accountId !== 'string' ) return res.status(HttpStatusCodes.BAD_REQUEST).send('Invalid request')
+
     const nfts = await alchemyClient.nft.getNftsForOwner(process.env.VISTA_CINEMEX_PUBLIC_KEY)
+    const nftsByAddress = nfts.ownedNfts.filter(nft => nft.contract.address === contractAddress );
+    
+    const ticketTypeToFind = req.query.vip === 'true' ? "VIP" : "Standard";
+    
+    console.log(nftsByAddress)
 
-    const ticketTypeToFind = req.query.vip ? "VIP" : "Standard";
-
-    const vistaNfts = nfts.ownedNfts.filter(nft => nft.contract.address === req.query.address ).find(nft => nft.rawMetadata.
-      attributes.includes(att => att.trait_type === "Ticket type" && att.value ===  ticketTypeToFind ));
-    //Filter by isVip;
-
-    const nftToMove = vistaNfts[0];
-    console.log('---',nftToMove);
-
-    // Provider
+    const nftTokenIdToTransfer = nftsByAddress.find(
+      nft => nft.rawMetadata.attributes.some(att => att.trait_type === "Ticket type" && att.value === ticketTypeToFind)
+    );
+ 
     const alchemyProvider = new ethers.providers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY);
     const signer = new ethers.Wallet(process.env.VISTA_CINEMEX_PRIVATE_KEY, alchemyProvider);
-    const contract = await ethers.ContractFactory.getContract('0xcd2818ee9a42d53e1e24d8faa484b48bdbe4e93c', abi, signer)
+    const contract = await ethers.ContractFactory.getContract(contractAddress, abi, signer)
     
-    const test = await contract.transferFrom(process.env.VISTA_CINEMEX_PUBLIC_KEY, '0x2af4BCD6d84b0C9e68c2Db4394a42cf177f9f3B4',2);
-    console.log(test);
-    res.json(test);
+    const result = await contract.transferFrom(
+      process.env.VISTA_CINEMEX_PUBLIC_KEY, 
+      accountId,
+      nftTokenIdToTransfer.tokenId
+    );
+    
+    res.json(result);
   } catch (err: any) {
     console.error(err.message);
     res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send('Server Error');
